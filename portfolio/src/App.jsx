c505711ff92ef86a68b0ee5d17c063e7ced00291
Apurva Prakash <mailapurva007@gmail.com>
2024-01-15 03:01:43 +0530

    navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,27 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import Portfolio from "./Pages/Portfolio";
+import About from "./Pages/About-us";
 
 function App() {
-
-
   return (
     <>
-      <h1>Hello</h1>
-      <h2>Mani</h2>
+      <BrowserRouter>
+        <Nav activeClass={location.pathname}/>
+        <Routes>
+          <Route path="*" Component={Home} />
+          <Route path="/contact" Component={Contact} />
+          <Route path="/portfolio" Component={Portfolio} />
+          <Route path="/about-us" Component={About} />
+        </Routes>
+        <h1>Hello</h1>
+        <h2>Mani</h2>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,14 +1,51 @@
-// import React from 'react'
-import "Nav.css"
+import {useState} from 'react'
+import { Link, useLocation } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
+  const [showNav, setShowNav] = useState(false);
+  const location=useLocation();
+  console.log(location)
   return (
     <div>
-        <div className='navbar-container'>
-
+      <div className="navbar-container">
+        <div className="logo">
+          <h1>My logo</h1>
         </div>
+        <button onClick={() => setShowNav(!showNav)}>Click</button>
+        <ul>
+          <Link to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+            <li>Home</li>
+          </Link>
+          <Link to="/about-us">
+            <li>About</li>
+          </Link>
+          <Link to="/portfolio">
+            <li>Portfolio</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact us</li>
+          </Link>
+        </ul>
+        <div className="mob-links-container">
+            {showNav && <ul className='mob-links'>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about-us">
+              <li>About</li>
+            </Link>
+            <Link to="/portfolio">
+              <li>Portfolio</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact us</li>
+            </Link>
+          </ul>}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
